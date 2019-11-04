018ee3d2f3dca747ae39ebf1a46bea8080f3148f
Anthony La Porte <ant@ukbsd.org>
2019-11-04 22:45:25 +0000

    make all functions top level

--- src/js/logistics-tools.js
+++ src/js/logistics-tools.js
@@ -1,65 +1,58 @@
-let switchContainers = {
-  swapContainers: () => {
-    let result = {}
-    let source = $('#oldCtr').val().trim()
-    let dest = $('#newCtr').val().trim()
-    let id = Number(source.replace(/\D/g, ''))
-
-    result.oldContainer = source
-    result.newContainer = dest
-    result.consignments = []
-
-    $.getJSON('/trunkcontainers/' + id + '/events')
-    .done((events) => {
-      let cons = []
-      $.each(events, (_i, e) => {
-        let bc = e.description.split(' ')[1]
-        if (bc.match(/^PCS[0-9]{9}$/)) cons.push(bc)
+let scMain = () => {
+  let result = {}
+  let source = $('#oldCtr').val().trim()
+  let dest = $('#newCtr').val().trim()
+  let id = Number(source.replace(/\D/g, ''))
+  result.oldContainer = source
+  result.newContainer = dest
+  result.consignments = []
+  $.getJSON('/trunkcontainers/' + id + '/events')
+  .done((events) => {
+    let cons = []
+    $.each(events, (_i, e) => {
+      let bc = e.description.split(' ')[1]
+      if (bc.match(/^PCS[0-9]{9}$/)) cons.push(bc)
+    })
+    return cons
+  })
+  .then((records) => {
+    if (records.length !== 0) {
+      $.each(records, (_i, record) => {
+        $.post('/trunkcontainers/' + dest + '/scan/' + record)
+          .done((response) => {
+            let r = { barcode: record, status: response.status }
+            result.consignments.push(r)
+          })
       })
-      return cons
-    })
-    .then((records) => {
-      if (records.length !== 0) {
-        $.each(records, (_i, record) => {
-          $.post('/trunkcontainers/' + dest + '/scan/' + record)
-            .done((response) => {
-              let r = { barcode: record, status: response.status }
-              result.consignments.push(r)
-            })
-        })
-      } else {
-        result.error = 1
-        result.errormessage = 'Could not get records for ' + source
-      }
-      $('#switch-results').text(JSON.stringify(result, undefined, 2))
-    })
-  },
-  prepare: () => {
-    let tcregex = '(CSTC|OOC)[0-9]{8}'
-
-    let form = $('<div>', { id: 'cForm' })
-      .append($('<input>', {
-        id: 'oldCtr',
-        required: 'required',
-        pattern: tcregex
-      }))
-      .append($('<input>', {
-        id: 'newCtr',
-        required: 'required',
-        pattern: tcregex
-      }))
-      .append($('<button>', {
-        text: 'Move Records',
-        onclick: switchContainers.swapContainers
-      }))
-
-    $('#ltInsert')
-      .empty()
-      .append(form)
-      .append($('<div>', { id: 'switch-results' }))
-
-    $('#oldCtr').focus()
-  }
+    } else {
+      result.error = 1
+      result.errormessage = 'Could not get records for ' + source
+    }
+    $('#switch-results').text(JSON.stringify(result, undefined, 2))
+  })
+}
+let scPrepare = () => {
+  let tcregex = '(CSTC|OOC)[0-9]{8}'
+  let form = $('<div>', { id: 'cForm' })
+    .append($('<input>', {
+      id: 'oldCtr',
+      required: 'required',
+      pattern: tcregex
+    }))
+    .append($('<input>', {
+      id: 'newCtr',
+      required: 'required',
+      pattern: tcregex
+    }))
+    .append($('<button>', {
+      text: 'Move Records',
+      onclick: scMain
+    }))
+  $('#ltInsert')
+    .empty()
+    .append(form)
+    .append($('<div>', { id: 'switch-results' }))
+  $('#oldCtr').focus()
 }
 
 function addPartsToDOM(){
@@ -73,23 +66,22 @@ function addPartsToDOM(){
   })
   .appendTo($('head'))
 
-
   var ltContainer = $('<div>', {
     id: 'ltContainer'
   })
   .append($('<button>', {
     class: 'ltButton',
-    onclick: consInspector,
+    // onclick: consInspector,
     text: 'Consignments Inspector'
   }))
   .append($('<button>', {
     class: 'ltButton',
-    onclick: autoContainers,
+    // onclick: autoContainers,
     text: 'Auto Containers'
   }))
   .append($('<button>', {
     class: 'ltButton',
-    onclick: sc.prepare,
+    onclick: scPrepare,
     text: 'Swap Containers'
   }))
   .append($('<div>', {id: 'ltInsert'}))
