b3e0905955ee0400ffb308ce34cb3f89df652f3c
Anthony La Porte <ant@ukbsd.org>
2019-11-04 22:33:15 +0000

    bring functionality together

--- src/js/logistics-tools.js
+++ src/js/logistics-tools.js
@@ -1,4 +1,66 @@
-import sc from 'https://dvere.github.io/logistics-tools/js/switchcontainers.min.js'
+let switchContainers = {
+  swapContainers: () => {
+    let result = {}
+    let source = $('#oldCtr').val().trim()
+    let dest = $('#newCtr').val().trim()
+    let id = Number(source.replace(/\D/g, ''))
+
+    result.oldContainer = source
+    result.newContainer = dest
+    result.consignments = []
+
+    $.getJSON('/trunkcontainers/' + id + '/events')
+    .done((events) => {
+      let cons = []
+      $.each(events, (_i, e) => {
+        let bc = e.description.split(' ')[1]
+        if (bc.match(/^PCS[0-9]{9}$/)) cons.push(bc)
+      })
+      return cons
+    })
+    .then((records) => {
+      if (records.length !== 0) {
+        $.each(records, (_i, record) => {
+          $.post('/trunkcontainers/' + dest + '/scan/' + record)
+            .done((response) => {
+              let r = { barcode: record, status: response.status }
+              result.consignments.push(r)
+            })
+        })
+      } else {
+        result.error = 1
+        result.errormessage = 'Could not get records for ' + source
+      }
+      $('#switch-results').text(JSON.stringify(result, undefined, 2))
+    })
+  },
+  prepare: () => {
+    let tcregex = '(CSTC|OOC)[0-9]{8}'
+
+    let form = $('<div>', { id: 'cForm' })
+      .append($('<input>', {
+        id: 'oldCtr',
+        required: 'required',
+        pattern: tcregex
+      }))
+      .append($('<input>', {
+        id: 'newCtr',
+        required: 'required',
+        pattern: tcregex
+      }))
+      .append($('<button>', {
+        text: 'Move Records',
+        onclick: switchContainers.swapContainers
+      }))
+
+    $('#ltInsert')
+      .empty()
+      .append(form)
+      .append($('<div>', { id: 'switch-results' }))
+
+    $('#oldCtr').focus()
+  }
+}
 
 function addPartsToDOM(){
 
@@ -11,33 +73,20 @@ function addPartsToDOM(){
   })
   .appendTo($('head'))
 
-  // $('body')
-  // .append($('<script>', {
-  //   id: 'lt-ci',
-  //   src: lt + 'js/consinspect.min.js?v=' + $.now()
-  // }))
-  // .append($('<script>', {
-  //   id: 'lt-ac',
-  //   src: lt + 'js/autocontainers.min.js?v=' + $.now()
-  // }))
-  // .append($('<script>', {
-  //   id: 'lt-sc',
-  //   src: 'js/switchcontainers.min.js?v=' + $.now()
-  // }))
 
   var ltContainer = $('<div>', {
     id: 'ltContainer'
   })
-  // .append($('<button>', {
-  //   class: 'ltButton',
-  //   onclick: consInspector,
-  //   text: 'Consignments Inspector'
-  // }))
-  // .append($('<button>', {
-  //   class: 'ltButton',
-  //   onclick: autoContainers,
-  //   text: 'Auto Containers'
-  // }))
+  .append($('<button>', {
+    class: 'ltButton',
+    onclick: consInspector,
+    text: 'Consignments Inspector'
+  }))
+  .append($('<button>', {
+    class: 'ltButton',
+    onclick: autoContainers,
+    text: 'Auto Containers'
+  }))
   .append($('<button>', {
     class: 'ltButton',
     onclick: sc.prepare,
